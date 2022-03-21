ddb9d71f2bab8c17cae03562591b9e6a334e519e
Tatiane Pereira Queiroz <37455978+Tatipq@users.noreply.github.com>
2022-03-21 16:37:57 -0300

    imagem
    definição de img

--- qrcode-reactjs/src/App.js
+++ qrcode-reactjs/src/App.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 import QRCodeCanvas from './QRCodeCanvas';
-let imgQrcode = './assets/logo.png';
+import imgQrcode from './assets/logo.png';
 
 function App() {
 
   const [text, setText] = useState("")
   return (  
     <div className="App"  id="App">
-      <a href="App.js" ><img src={imgQrcode}width="100" height="40"></img></a>
+      <a href="App.js" ><img src={imgQrcode}width="150" height="150"></img></a>
       <input onChange={(element)=>{setText(element.target.value)}} value={text}></input>
       <QRCodeCanvas text={text}></QRCodeCanvas>
     </div>
